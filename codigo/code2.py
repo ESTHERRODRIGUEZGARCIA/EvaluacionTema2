262d5d2e374401415e5bf082d7602f4e6dc57c97
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-07 15:59:51 +0200

    duda

---
+++ codigo/code2.py
@@ -0,0 +1,13 @@
+class Alumno():
+    def __init__(self, nombre, nota):
+        self.nombre = nombre
+        self.nota = nota
+        print("El alumno se ha creado con éxito. ")
+
+    def __str__(self):
+        return 
+    def calificacion(self):
+        if self.nota >= 5 and self.nota <=10:
+            print("Alumno aprobado. ")
+        elif self.nota < 5:
+            print("Alumno suspenso. ")
