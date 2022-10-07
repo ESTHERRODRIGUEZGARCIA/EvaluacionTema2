0a36a01688be17c0ab17e4a5098d72dafe0af96a
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-07 15:46:02 +0200

    alumno aprobado/suspenso

--- codigo.py
+++ codigo.py
@@ -1,5 +1,13 @@
-class alumno():
+class Alumno():
     def __init__(self, nombre, nota):
         self.nombre = nombre
         self.nota = nota
+        print("El alumno se ha creado con éxito. ")
+
+    def calificacion(nota):
+        if nota >= 5:
+            print("Alumno aprobado. ")
+        elif nota < 5:
+            print("Alumno suspenso. ")
+            
         
