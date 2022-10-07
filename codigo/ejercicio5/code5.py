d1805314b815129acd6ae4cdc94c1c8730e7fcfa
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-07 17:03:41 +0200

    subclases coche y moto hechas

--- codigo/ejercicio5/code5.py
+++ codigo/ejercicio5/code5.py
@@ -24,6 +24,25 @@ class Coche(Vehiculo):
     def get_cilindrada(self):
         return self.cilindrada
 
+class Motocicleta(Vehiculo):
+    def __init__(self, color, ruedas, velocidad, cilindrada):
+        Vehiculo.__init__(self, color, ruedas)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+
+    def __str__(self):
+        return Vehiculo.__str__(self) + ", {} km/h, {} cc".format(self.velocidad, self.cilindrada)
+
+    def set_velocidad(self, velocidad):
+        self.velocidad = velocidad
+    def get_velocidad(self):
+        return self.velocidad
+
+    def set_cilindrada(cilindrada, self):
+        self.cilindrada = cilindrada
+    def get_cilindrada(self):
+        return self.cilindrada
+
 class Bicicleta(Vehiculo):
     def __init_subclass__(cls) -> None:
         return super().__init_subclass__()
@@ -32,7 +51,7 @@ class Bicicleta(Vehiculo):
         return super().__str__()
     
     def set_tipo():
-        
+
 
 class Camioneta(Vehiculo):
     def 
