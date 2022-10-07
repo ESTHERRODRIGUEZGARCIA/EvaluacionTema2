9c9dc2153962b0a626067657f154302a7a85dddc
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-07 16:11:44 +0200

    alumno más nota

--- lanzador.py
+++ lanzador.py
@@ -3,4 +3,4 @@ from codigo.code1 import calificacion
 def elegir():
     eleccion = (" ¿Qué ejercicio quieres realizar?")
     if eleccion == 1:
-        return code1()
+        return codigo.code1()
---
+++ main.py
@@ -0,0 +1,3 @@
+from lanzador import elegir
+if __name__ == "__main__":
+    elegir()
