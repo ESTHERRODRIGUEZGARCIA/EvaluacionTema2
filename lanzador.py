2bf855bbda810fad7a5e926c6e9e0e799a800fc8
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-07 16:17:39 +0200

    no entiendo lo de codigo is not defined??

--- lanzador.py
+++ lanzador.py
@@ -1,6 +1,6 @@
 from codigo.code1 import calificacion
 
 def elegir():
-    eleccion = (" ¿Qué ejercicio quieres realizar?")
+    eleccion = ("¿Qué ejercicio quieres realizar?")
     if eleccion == 1:
         return codigo.code1()
