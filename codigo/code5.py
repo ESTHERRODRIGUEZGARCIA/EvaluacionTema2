6dec2a00ad804daeae45cff34b841e72ea6a5add
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-10-07 16:31:38 +0200

    subclase

--- codigo/code5.py
+++ codigo/code5.py
@@ -5,3 +5,11 @@ class Vehiculo():
     def __str__(self):
         return "Color {}, {} ruedas".format( self.color, self.ruedas )
 
+class Coche(Vehiculo):
+    def __init__(self, color, ruedas, velocidad, cilindrada):
+        Vehiculo.__init__(self, color, ruedas)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+
+    def __str__(self):
+        return Vehiculo.__str__(self) + ", {} km/h, {} cc".format(self.velocidad, self.cilindrada)
